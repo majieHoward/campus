675af98bbfe43466ab874254abb174b5e39e3feb
majieHoward <120234740@qq.com>
2017-05-27 15:48:28 +0800

    显示图片效果

--- js/photoControl.js
+++ js/photoControl.js
@@ -14,8 +14,11 @@ mui('.mui-scroll').on('tap','.mui-control-item:not(.mui-active)',function(){
     
 });
 var imgSource;
+var imgSourceSource;
+
 var evaluateImgSource=function(imgSource){
     this.imgSource=imgSource;
+    this.imgSourceSource=imgSource;
 }
 
 var obtainImgSource=function(){
@@ -23,16 +26,20 @@ var obtainImgSource=function(){
 }
 var loadPhotoControlImg=function(imgSoucre){
     evaluateImgSource(imgSoucre);
-    var img = new Image(),
-    div = document.createElement('div');
-    div.appendChild(img);
-    img.id="processedPicture";
-    img.name="processedPicture";
-    img.onload = function () {
-        document.querySelector('#upload-container').appendChild(div);
-    };     
-    img.src = imgSoucre;
+    var img = new Image();
     
+    img.src = imgSoucre;
+    img.onload = function () {
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        canvas.width = img.width;//////////重点  
+        canvas.height = img.height;//////////重点  
+        ctx.clearRect(0,0,canvas.width,canvas.height);  
+        ctx.drawImage(img,0,0,canvas.width,canvas.height); 
+        //图片以原先尺寸呈现 
+        canvas.style.width = '100%';//////////重点  
+        //ctx.drawImage(img, 0, 0,width,height,0,0,width,height);
+    };     
     //document.getElementById("processedPicture").src=imgSoucre;
 }
 var subPageProcessing=function(){
@@ -42,6 +49,7 @@ var subPageProcessing=function(){
 var gotobackFeedback=document.getElementById("gotobackFeedback");
 
 gotobackFeedback.addEventListener('tap',function(){
+
     document.getElementById("upload-container").innerHTML="";
     window.parent.switchToFeedback();
 });
@@ -49,7 +57,23 @@ gotobackFeedback.addEventListener('tap',function(){
 /**完成了图片的改造**/
 var completeFeedback=document.getElementById("completeFeedback");
 completeFeedback.addEventListener('tap',function(){
-    document.getElementById("upload-container").innerHTML="";
+    var uploadContainer=document.getElementById("upload-container");
+    uploadContainer.innerHTML="";
+    var canvas = document.createElement('canvas'); 
+    canvas.id = "canvas"; 
+    uploadContainer.appendChild(canvas);
     window.parent.switchToFeedback(obtainImgSource());
+});
+
+
+
+mui("#filterProcessingList").on('tap', 'a', function (event) {
+    var filterMethod=this.getAttribute("filterMethod");
+    var img= document.getElementById("processedPicture");
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    
+    var imageData = ctx.getImageData(0,0,canvas.width, canvas.height);
+    ctx.putImageData(negativeFilter(imageData),0,0);  
 });
 
